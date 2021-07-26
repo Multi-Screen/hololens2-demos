445f271e857f329fdf25b137a80b1f7a88c51428
shoukailiang <shoukailiang@qq.com>
2021-07-26 20:23:42 +0800

    fix single eyes

--- Assets/ImageScript.cs
+++ Assets/ImageScript.cs
@@ -17,10 +17,38 @@ public class ImageScript : MonoBehaviour
     }
 
     public void getOne(){
-        print("get");
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
     }
 
     public void lostOne(){
-        print("lostOne");
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
     }
 }
